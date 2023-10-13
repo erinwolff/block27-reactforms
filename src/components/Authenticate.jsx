9d03852f1c00ce60f6196bf83801705be5662437
Erin Wolff <erin_wolff@outlook.com>
2023-10-13 12:22:26 -0700

    completed SignUpForm component

---
+++ src/components/Authenticate.jsx
@@ -0,0 +1,10 @@
+export default function Authenticate() {
+
+  return (
+    <>
+      <h2>Authenticate</h2>
+
+    </>
+  )
+
+}
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export default function SignUpForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const signup = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+        method: "POST",
+        body: JSON.stringify({ username, password }),
+      });
+      const response = await signup.json();
+      console.log(response);
+    } catch (error) {
+      setError(error.message)
+    }
+  }
+
+  return (
+    <>
+      <h2>Sign Up</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username: <input value={username} onChange={(e) => { setUsername(e.target.value); }} />
+        </label>
+        <label>
+          Password: <input value={password} onChange={(e) => { setPassword(e.target.value); }} />
+        </label>
+        <button>Submit</button>
+
+
+
+      </form>
+
+    </>
+  )
+}
