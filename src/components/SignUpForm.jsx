b55e38eb6c1013525700e40340d9fa75a6518768
Erin Wolff <erin_wolff@outlook.com>
2023-10-13 15:17:10 -0700

    added button to clear form

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
-export default function SignUpForm({setToken}) {
+export default function SignUpForm({ setToken }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
-  
+
   async function handleSubmit(e) {
     e.preventDefault();
 
-    if(username.length < 5 || password.length < 8){
+    if (username.length < 5 && username.length > 1 || password.length < 8 && password.length > 1) {
       setError("Username must be at least 5 characters and password must be at least 8 characters");
       return;
     }
@@ -16,7 +16,7 @@ export default function SignUpForm({setToken}) {
     try {
       const signup = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
         method: "POST",
-        headers: {"Content-Type": "application/json"},
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
       });
       const response = await signup.json();
@@ -34,12 +34,17 @@ export default function SignUpForm({setToken}) {
         <label>
           Username: <input value={username} onChange={(e) => { setUsername(e.target.value); }} />
         </label>
+        <br />
+        <h6>*Username must contain at least 5 characters.</h6>
         <label>
           Password: <input value={password} onChange={(e) => { setPassword(e.target.value); }} />
         </label>
+        <br />
+        <h6>*Password must contain at least 8 characters.</h6>
         <button>Submit</button>
-
-
+        <br />
+        <br />
+        <button onClick={() => (setUsername(""), setPassword(""))}>Clear Form</button>
 
       </form>
 
